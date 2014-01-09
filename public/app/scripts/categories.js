029cf24f85a6c56b632f52f7e2f7fbc449af20c8
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2014-01-09 09:11:10 -0500

    imported all categories and subcategories

---
+++ public/app/scripts/categories.js
@@ -0,0 +1,77 @@
+var categories =
+	[{
+	"Shiurim": [
+		/*==========  Bereishit  ==========*/
+		"Bereishit",
+		"Noach",
+		"Lech Lecha",
+		"Vayeira",
+		"Chayei Sarah",
+		"Toldot",
+		"Vayeitzei",
+		"Vayishlach",
+		"Vayeishev",
+		"Mikeitz",
+		"Vayigash",
+		"Vayechi",
+		/*==========  Shemos  ==========*/
+
+		"Shemot",
+		"Va'eira",
+		"Bo",
+		"Beshalach",
+		"Yitro",
+		"Mishpatim",
+		"Terumah",
+		"Tetzaveh",
+		"Ki Tisa",
+		"Vayakhel",
+		"Pekudei",
+		/*==========  Vayikra  ==========*/
+		"Vayikra",
+		"Tzav",
+		"Shemini",
+		"Tazria",
+		"Metzora",
+		"Acharei",
+		"Kedoshim",
+		"Emor",
+		"Behar",
+		"Bechukotai",
+		/*==========  Bamidbar  ==========*/
+		"Bamidbar",
+		"Naso",
+		"Behaalotecha",
+		"Shelach",
+		"Korach",
+		"Chukat",
+		"Balak",
+		"Pinchas",
+		"Matot",
+		"Massei",
+		/*==========  Devarim  ==========*/
+		"Devarim",
+		"Va'etchanan",
+		"Eikev",
+		"Re'eh",
+		"Shoftim",
+		"Ki Teitzei",
+		"Ki Tavo",
+		"Nitzavim",
+		"Vayelech",
+		"Haazinu",
+		"V'Zot HaBerachah",
+		/*==========  Other Shiurim ==========*/
+		"Davening",
+		"Tehillim",
+		"Laws of Brochos"
+	],
+	"Music": [
+		"Serve Hashem with Song",
+		"On a Personal Note"
+	],
+	"Leading Services": [
+		"Friday Night",
+		"Weekday Prayers"
+	]
+}];
